20cad6159aa02136fa731a33cee73b2d4fee2efd
Kryštof-Mikuláš Štys <493159@mail.muni.cz>
2023-05-13 12:30:55 +0200

    [Feat] Printout example

--- DatabaseFirst/Program.cs
+++ DatabaseFirst/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DatabaseFirst.Models;
+using Microsoft.EntityFrameworkCore;
+
+using (OfficeContext dbContext = new())
+{
+    IQueryable<Person> people = dbContext.People
+        .Include(x => x.Address)
+        .Include(x => x.Companies)
+        .ThenInclude(x => x.Offices)
+        .ThenInclude(x => x.Address);
+
+    await dbContext.People
+            .Select(x => x.Name)
+            .ForEachAsync(Console.WriteLine);
+
+    await dbContext.Offices
+            .Select(x => x.Address.Country)
+            .ForEachAsync(Console.WriteLine);
+}
--- ModelFirst/Program.cs
+++ ModelFirst/Program.cs
@@ -4,36 +4,91 @@ using Microsoft.EntityFrameworkCore;
 
 using (ExampleDbContext exampleDbContext = new())
 {
-    exampleDbContext.Database.MigrateAsync();
+    exampleDbContext.Database.EnsureCreated();
 
     // Create a new address
-    Address address = new()
+    Address addressCompany1 = new()
     {
         Street = "Street",
         City = "City",
-        ZipCode = "ZipCode",
-        Country = "Country",
-        Number = "Number"
+        ZipCode = "477 12",
+        Country = "Chechnya",
+        Number = "32/a"
+    };
+
+    Address addressCompany2 = new()
+    {
+        Street = "Street",
+        City = "Paříž",
+        ZipCode = "477 12",
+        Country = "Czechia",
+        Number = "891"
+    };
+
+    Address addressEmployee = new()
+    {
+        Street = "Street",
+        City = "City",
+        ZipCode = "477 12",
+        Country = "Chechnya",
+        Number = "32/b"
+    };
+
+    Office officeCzechia = new()
+    {
+        Address = addressCompany2
     };
 
     // Create a new company
     Company company = new()
     {
         Employees = new List<Person>()
+        { 
+            new() 
+            { 
+                Name = "Tomáš", 
+                Surname = "Pech", 
+                BirthNumber = 1234561234,
+                Email = "tomas@pe.ch",
+                Address = addressEmployee,
+                Companies = new List<Company>()
+                {
+                    new Company
+                    {
+                        Offices = new List<Office>()
+                        {
+                            officeCzechia
+                        }
+                    }
+                }
+            }
+        }
     };
 
     // Create a new office
-    Office office = new()
+    Office officeChechnya = new()
     {
         Company = company,
-        Address = address
+        Address = addressCompany1
     };
 
     // Add the office to the database
-    exampleDbContext.Offices.Add(office);
+    exampleDbContext.Offices.Add(officeChechnya);
 
     // Commit
     exampleDbContext.SaveChanges();
 
-    Console.WriteLine(exampleDbContext.People.Include(x => x.Address).Include(x => x.Companies).ToList());
+    IQueryable<Person> people = exampleDbContext.People
+        .Include(x => x.Address)
+        .Include(x => x.Companies)
+        .ThenInclude(x => x.Offices)
+        .ThenInclude(x => x.Address);
+
+    await exampleDbContext.People
+            .Select(x => x.Name)
+            .ForEachAsync(Console.WriteLine);
+
+    await exampleDbContext.Offices
+            .Select(x => x.Address.Country)
+            .ForEachAsync(Console.WriteLine);
 }
