a80a823479d0e2f96678085430446cb0bb346133
Kryštof-Mikuláš Štys <493159@mail.muni.cz>
2023-05-13 12:34:45 +0200

    [Enh] Annotations examples

--- ModelFirst/Model/Person.cs
+++ ModelFirst/Model/Person.cs
@@ -1,9 +1,15 @@
-﻿namespace ModelFirst.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelFirst.Model
 {
     public class Person
     {
+        [Key]
         public Guid Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [MinLength(1)]
+        [StringLength(50)]
         public string Surname { get; set; }
         public string Email { get; set; }
         public int BirthNumber { get; set; }
